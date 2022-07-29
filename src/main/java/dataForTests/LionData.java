08a1354b7cdc3acb82e9b87d840b29c55445746e
Katerina Ushakova <ush.aniretake@gmail.com>
2022-07-29 22:34:53 +0300

    reverted

---
+++ src/main/java/dataForTests/LionData.java
@@ -0,0 +1,11 @@
+package dataForTests;
+
+public class LionData {
+
+    public static final Object[][] LION_HAS_MANE_DATA = {
+            {"Самец", true},
+            {"Самка", false}
+    };
+
+    public static final int DEFAULT_LION_KITTENS = 1;
+}
