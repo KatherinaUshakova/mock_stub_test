114bd958a6cfdefb22a1df265ae777ee3634750b
Katerina Ushakova <ush.aniretake@gmail.com>
2022-08-02 22:46:11 +0300

    exception test

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -5,7 +5,9 @@ import dataForTests.LionData;
 import org.junit.Assert;
 import org.junit.Before;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
@@ -36,4 +38,23 @@ public class LionTest {
         Mockito.when(feline.getFood(AnimalKindFoodType.ANIMAL_KIND_PREDATOR)).thenReturn(AnimalKindFoodType.PREDATOR_FOOD);
         Assert.assertEquals(lion.getFood(), AnimalKindFoodType.PREDATOR_FOOD);
     }
+
+    @Test
+    public void lionConstructorThrowsExceptionWhenInitialisedWithWrongSex(){
+        Assert.assertThrows(Exception.class, () -> new Lion("stone", feline));
+    }
+
+//    @Rule
+//    public ExpectedException thrown = ExpectedException.none();
+//    @Test
+//    public void lionConstructorThrowsExceptionWhenInitialisedWithWrongSex2() throws Exception{
+//        thrown.expect(Exception.class);
+//        thrown.expectMessage("Используйте допустимые значения пола животного - самец или самка");
+//        new Lion("Самей", feline);
+//    }
+//
+//    @Test(expected = Exception.class)
+//    public void lionConstructorThrowsExceptionWhenInitialisedWithWrongSex3() throws Exception{
+//        new Lion("Самеq", feline);
+//    }
 }
